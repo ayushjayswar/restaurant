7859347bd43f1eb7224d4c5d7b45de1f9b30fe53
Ayush Jayswar <ayushjayswra@gmail.com>
2026-09-19 12:45:21 +0530

    route update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -125,23 +125,23 @@ const Footer = () => {
                         <ul className='grid grid-cols-[auto_auto] justify-start gap-x-2 gap-y-2 font-semibold text-xl text-white'>
 
                             <li>
-                                <a href="#home">Home</a>
+                                <a href="/home">Home</a>
                             </li>
 
                             <li>
-                                <a href="#about">About</a>
+                                <a href="/about">About</a>
                             </li>
 
                             <li>
-                                <a href="#menu">Menu</a>
+                                <a href="/menu">Menu</a>
                             </li>
 
                              <li>
-                                <a href="#room-booking">Room</a>
+                                <a href="/room-booking">Room</a>
                             </li>
 
                             <li>
-                                <a href="#reservation">Reservation</a>
+                                <a href="/reservation">Reservation</a>
                             </li>
 
                         </ul>
