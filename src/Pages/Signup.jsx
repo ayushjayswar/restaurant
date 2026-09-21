8cb52db8f1d7f9c969633538c97c05ef8760fcce
Ayush Jayswar <ayushjayswra@gmail.com>
2026-09-21 15:21:28 +0530

    again url change for login or signin

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -7,7 +7,8 @@ const BG_IMAGE =
 
 // FastAPI backend ka URL - agar backend kisi doosre port/host par ho to yahan badal do
 // const API_URL = "http://localhost:8000";
-const API_URL = "http://15.206.149.37:8080";
+// const API_URL = "http://15.206.149.37:8080";
+const API_URL = "https://lcd-dressing-jim-oven.trycloudflare.com"
 
 export default function Signup({ onSignupSuccess }) {
   const navigate = useNavigate();
